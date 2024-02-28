234398430da7d5b820dbb4cc480b0ae20defe848
tamakiseda <78771577+tamakiseda@users.noreply.github.com>
2024-02-28 09:59:18 +0800

    Update streamlit_app.py
    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,32 @@
 
 import streamlit as st
 from streamlit_image_select import image_select
-
+st.image("./Westernacher_Logo_3.png")
 st.markdown("""
     ## Westernacher AI Navigator
 
 #             """)
+
 # img = image_select("Machine Learning", ["classification.png", "regression.png", "clustering.png"])
 # st.write(img)
 #captions=["ABC/XYZ AI Clustering", "Customer AI Clustering", "Others"]
 #captions=["Feature Engineering", "Data Exploratory Analysis", "Others"]
 #captions=["Planning Bot", "Private LLM", "Others"]
 
-img1 = image_select("Segmentation", ["abc.png", "customer.png", "other.png"])
+img1 = image_select("Segmentation", ["category.png", "customerai.png", "other.png"])
 image_urls = {
-    "abc.png": "https://xyz-segmentation-westernacher.streamlit.app/",
-    "customer.png": "https://xyz-segmentation-westernacher.streamlit.app/",
+    "category.png": "https://xyz-segmentation-westernacher.streamlit.app/",
+    "customerai.png": "https://xyz-segmentation-westernacher.streamlit.app/",
     "other.png": "https://xyz-segmentation-westernacher.streamlit.app/"
 }
 url = image_urls.get(img1, "")
 #st.image(img)
 st.markdown(f"[🔗]({url})")
 
-img2 = image_select("Demand Sensing", ["feature.png", "DataExploratoryAnalysis.png", "other.png"])
+img2 = image_select("Demand Sensing", ["featureee.png", "dea.png", "other.png"])
 image_urls = {
-    "feature.png": "https://xyz-segmentation-westernacher.streamlit.app/",
-    "DataExploratoryAnalysis.png": "https://xyz-segmentation-westernacher.streamlit.app/",
+    "featureee.png": "https://xyz-segmentation-westernacher.streamlit.app/",
+    "dea.png": "https://xyz-segmentation-westernacher.streamlit.app/",
     "other.png": "https://xyz-segmentation-westernacher.streamlit.app/"
 }
 url = image_urls.get(img2, "")
